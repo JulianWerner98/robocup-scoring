2edfd7522e55047de1af773801889c8b0182f59a
Julian Werner <info@werner-julian.de>
2022-12-16 14:34:22 +0100

    chore: add runner and change port

--- backend/src/config/config.validator.ts
+++ backend/src/config/config.validator.ts
@@ -1,7 +1,7 @@
 import * as Joi from "joi";
 
 export const validationSchema = Joi.object({
-    PORT: Joi.number().default(3000),
+    PORT: Joi.number().default(3001),
     MONGO_URI: Joi.string().default('mongodb://mongoDB:27017/robocup'),
     KEYCLOAK_URI: Joi.string().default('https://keycloak.crashtec.de/auth'),
     KEYCLOAK_REALM: Joi.string().default('RoboCup'),
